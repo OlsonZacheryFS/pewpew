2a9a8b722af458d86a0730dc7daaa759ca542ec1
Zachery Olson <Zachery.Olson@FamilySearch.org>
2023-06-13 13:18:10 -0600

    Pre-compile script snippets to speed up repeated querying

--- lib/config/src/configv2/query.rs
+++ lib/config/src/configv2/query.rs
@@ -3,51 +3,58 @@ use boa_engine::{
     property::Attribute,
     Context, JsResult, JsValue,
 };
+use derivative::Derivative;
 use itertools::Itertools;
 use serde::Deserialize;
 use serde_json::Value as SJVal;
-use std::collections::{BTreeMap, VecDeque};
+use std::{
+    cell::{OnceCell, RefCell},
+    collections::{BTreeMap, VecDeque},
+};
 
-// TODO: check if strings can be precompiled here
-// mainly: can you precompile a JS code that references the property "response" and execute it
-// multiple times, even if "response" is a different value
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Deserialize, Derivative)]
+#[derivative(PartialEq, Eq)]
 pub struct Query {
     select: Select,
-    for_each: Vec<String>,
-    r#where: Option<String>,
+    for_each: Vec<CompileOnce>,
+    r#where: Option<CompileOnce>,
+    #[serde(skip, default = "get_context")]
+    #[derivative(PartialEq = "ignore")]
+    ctx: RefCell<Context>,
+}
+
+fn get_context() -> RefCell<Context> {
+    RefCell::from(Context::default()) // grab the scripting context if the functions are needed
 }
 
 impl Query {
     fn query(&self, request: SJVal, response: SJVal, stats: SJVal) -> Option<SJVal> {
-        // maybe cache the same context (see TODO above)
-        let mut ctx = Context::default(); // grab the scripting context if the functions are needed
+        let mut ctx = self.ctx.borrow_mut();
+        let ctx = &mut ctx;
         IntoIterator::into_iter([
             ("request", &request),
             ("response", &response),
             ("stats", &stats),
         ])
-        .map(|(n, o)| (n, JsValue::from_json(o, &mut ctx).unwrap()))
+        .map(|(n, o)| (n, JsValue::from_json(o, ctx).unwrap()))
         .collect::<Vec<_>>()
         .into_iter()
         .for_each(|(n, o)| ctx.register_global_property(n, o, Attribute::READONLY));
         self.r#where
             .as_ref()
-            .map_or(true, |w| ctx.eval(w).unwrap().as_boolean().unwrap())
+            .map_or(true, |w| w.execute_on(ctx).unwrap().as_boolean().unwrap())
             .then(|| {
                 let for_each: Vec<VecDeque<JsValue>> = self
                     .for_each
                     .iter()
-                    .map(|fe| ctx.eval(fe).unwrap())
+                    .map(|fe| fe.execute_on(ctx).unwrap())
                     .collect_vec()
                     .into_iter()
                     .map(|jv| match jv {
                         JsValue::Object(o) if o.is_array() => {
-                            let a = JsArray::from_object(o, &mut ctx).unwrap();
-                            let mut vd =
-                                VecDeque::with_capacity(a.length(&mut ctx).unwrap() as usize);
-                            std::iter::repeat_with(|| a.pop(&mut ctx).unwrap())
+                            let a = JsArray::from_object(o, ctx).unwrap();
+                            let mut vd = VecDeque::with_capacity(a.length(ctx).unwrap() as usize);
+                            std::iter::repeat_with(|| a.pop(ctx).unwrap())
                                 .take_while(|v| !v.is_null_or_undefined())
                                 .for_each(|v| vd.push_front(v));
                             vd
@@ -58,7 +65,7 @@ impl Query {
                 let for_each = for_each
                     .into_iter()
                     .multi_cartesian_product()
-                    .map(|v| JsArray::from_iter(v, &mut ctx).into())
+                    .map(|v| JsArray::from_iter(v, ctx).into())
                     .collect_vec();
                 let for_each = if for_each.is_empty() {
                     vec![JsValue::Undefined]
@@ -70,29 +77,59 @@ impl Query {
                     .into_iter()
                     .map(|x| {
                         ctx.register_global_property("for_each", x, Attribute::READONLY);
-                        self.select.select(&mut ctx).unwrap()
+                        self.select.select(ctx).unwrap()
                     })
                     .collect_vec();
-                SJVal::Array(
-                    data.into_iter()
-                        .map(|x| x.to_json(&mut ctx).unwrap())
-                        .collect(),
-                )
+                SJVal::Array(data.into_iter().map(|x| x.to_json(ctx).unwrap()).collect())
             })
     }
 }
 
-#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Deserialize, Derivative)]
+#[derivative(PartialEq, Eq)]
+struct CompileOnce(
+    String,
+    #[serde(skip)]
+    #[derivative(PartialEq = "ignore")]
+    OnceCell<gc::Gc<boa_engine::vm::CodeBlock>>,
+);
+
+impl From<String> for CompileOnce {
+    fn from(value: String) -> Self {
+        Self(value, OnceCell::new())
+    }
+}
+
+impl CompileOnce {
+    fn get(&self, ctx: &mut Context) -> gc::Gc<boa_engine::vm::CodeBlock> {
+        self.1
+            .get_or_init(|| {
+                use boa_engine::syntax::Parser;
+
+                let code = Parser::new(self.0.as_bytes()).parse_all(ctx).unwrap();
+                ctx.compile(&code).unwrap()
+            })
+            .clone()
+    }
+
+    fn execute_on(&self, ctx: &mut Context) -> JsResult<JsValue> {
+        let code = self.get(ctx);
+        ctx.execute(code)
+    }
+}
+
+#[derive(Debug, Deserialize, Derivative)]
+#[derivative(PartialEq, Eq)]
 #[serde(untagged)]
 enum Select {
-    Expr(String),
+    Expr(CompileOnce),
     Map(BTreeMap<String, Self>),
 }
 
 impl Select {
     fn select(&self, ctx: &mut Context) -> JsResult<JsValue> {
         match self {
-            Self::Expr(s) => ctx.eval(s),
+            Self::Expr(code) => code.execute_on(ctx),
             Self::Map(m) => {
                 let m: BTreeMap<&str, JsValue> = m
                     .iter()
@@ -116,9 +153,10 @@ mod tests {
     #[test]
     fn test_queries() {
         let q = Query {
-            select: Select::Expr("response.body.session".to_owned()),
-            r#where: Some("response.status < 400".to_owned()),
+            select: Select::Expr("response.body.session".to_owned().into()),
+            r#where: Some("response.status < 400".to_owned().into()),
             for_each: vec![],
+            ctx: get_context(),
         };
         let response = serde_json::json! { {"body": {"session": "abc123"}, "status": 200} };
         let res = q.query(SJVal::Null, response, SJVal::Null).unwrap();
@@ -128,12 +166,13 @@ mod tests {
             select: Select::Map(
                 [(
                     "name".to_owned(),
-                    Select::Expr("for_each[0].name".to_owned()),
+                    Select::Expr("for_each[0].name".to_owned().into()),
                 )]
                 .into(),
             ),
-            r#where: Some("true".to_owned()),
-            for_each: vec!["response.body.characters".to_owned()],
+            r#where: Some("true".to_owned().into()),
+            for_each: vec!["response.body.characters".to_owned().into()],
+            ctx: get_context(),
         };
         let response = serde_json::json! {
             {"body":    {
